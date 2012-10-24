25c30f8b6c16a306cea41867bb152c3028170e94
acer <alabitzke.mail@gmail.com>
2012-10-24 16:58:26 +0200

    vorläufige Endversion, die funktionieren sollte + Output für 8 Knoten generiert mit tcpdump

--- skripte/aufgabeEins.cc
+++ skripte/aufgabeEins.cc
@@ -20,22 +20,33 @@
 
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE("testWifi");
+NS_LOG_COMPONENT_DEFINE("aufgabeEins");
 
 int main (int argc, char *argv[]) {
 
 	LogComponentEnable("BulkSendApplication", LOG_LEVEL_INFO);
 
 	// Länge der Netzwerk-Topologie in m
-	unsigned lTopologie = 800;
-	//Abstand zweier Knoten in m
+	unsigned lTopologie = 350;
+	// Abstand zweier Knoten in m
 	unsigned deltaX = 50;
+	// Ausgabe-Ordner für die Tracing-Files
+	std::string outputDir = "gitFiles/output";
 
 	CommandLine cmd;
 	cmd.AddValue ("lTopologie", "Die Länge der Netzwerk-Topologie in m.", lTopologie);
 	cmd.AddValue ("deltaX", "Abstand zweier Knoten in m.", deltaX);
+	cmd.AddValue ("outputDir", "Ausgabe-Ordner für Tracing-Files", outputDir);
 	cmd.Parse (argc, argv);
 
+	// Deaktivierung der Fragmentierung
+	Config::SetDefault("ns3::WifiRemoteStationManager::FragmentationThreshold", StringValue("2000"));
+	// Deaktivierung des RtsCts-Mechanismus
+	Config::SetDefault("ns3::WifiRemoteStationManager::RtsCtsThreshold", StringValue("2000"));
+	// Paketlänge festlegen
+	Config::SetDefault("ns3::MinstrelWifiManager::PacketLength", StringValue("1460"));
+	// auch die TCP-Packetgröße muss hoch gesetzt werden, da sonst bei 536 liegend und die Packetgröße einschränkend
+	Config::SetDefault("ns3::TcpSocket::SegmentSize", StringValue("1460"));
 
 	// Anzahl Knoten
 	unsigned nNodes = lTopologie / deltaX + 1;
@@ -70,6 +81,7 @@ int main (int argc, char *argv[]) {
 	wifi.SetRemoteStationManager("ns3::MinstrelWifiManager");
 	//RtsCtsThreshold muss nicht gesetzt werden, da die Paketgröße kleiner als der Min-Wert ist
 	//FragmentationThreshold ebenfalls nicht
+
 	//802.11 g Modell festlegen
 	wifi.SetStandard(WIFI_PHY_STANDARD_80211g);
 
@@ -92,7 +104,7 @@ int main (int argc, char *argv[]) {
 	//FTP-Packet an letzten Knoten senden/Port 9
 	BulkSendHelper ftp = BulkSendHelper("ns3::TcpSocketFactory",InetSocketAddress (interfaces.GetAddress(nNodes-1), 9));
 	//Paketgröße festlegen
-	ftp.SetAttribute("MaxBytes", UintegerValue(1460));
+	ftp.SetAttribute("SendSize", UintegerValue(1460));
 	ApplicationContainer app = ftp.Install(nodes.Get(0));
 	app.Start(Seconds(0.0));
 	app.Stop(Seconds(30.0));
@@ -105,11 +117,11 @@ int main (int argc, char *argv[]) {
 	sinkApps.Stop (Seconds (30.0));
 
 	//Tracing aktivieren für alle Devices aller Knoten
-	wifiPhy.EnablePcap("output/testWifi", devices);
+	wifiPhy.EnablePcap( outputDir + "/aufgabeEins", devices);
 
 	//Routing-Tabelle, nur für Testzwecke
-	Ptr<OutputStreamWrapper> routingStream = Create<OutputStreamWrapper> ("output/testwifi.routes", std::ios::out);
-	      aodv.PrintRoutingTableAllEvery (Seconds (2), routingStream);
+	Ptr<OutputStreamWrapper> routingStream = Create<OutputStreamWrapper> ( outputDir + "/aufgabeEins.routes", std::ios::out);
+	aodv.PrintRoutingTableAllEvery (Seconds (1), routingStream);
 
 
 	//Simulation
